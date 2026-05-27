e70158191e9a0098a6446ac76db28ddfaa5b91be
ccorella111 <c.corella111@gmail.com>
2026-05-27 13:30:45 -0600

    feat(auth): add login validation logic

--- finanzas/views.py
+++ finanzas/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate
 
 # Create your views here.
 
@@ -6,9 +7,26 @@ def dashboard_view(request):
     
     return render(request,'finanzas/dashboard.html')
 
+
+
 def login_view(request):
 
+    if request.method == 'POST':
+        userCred = request.POST.get('username')
+        key = request.POST.get('password')
+
+        user = authenticate(request, username=userCred, password=key)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('dashboard')
+        else:
+            return render(request, 'auth/login.html',{'error' : 'Datos incorrectos'})
+        
     return render(request, 'auth/login.html')
+
+
 
 def register_view(request):
 
